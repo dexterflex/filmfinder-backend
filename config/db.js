937ff216feb2b0e943e429280497e576908efa00
dexterflex <vk281977@gmail.com>
2024-09-24 13:09:37 +0530

    hehe

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 
 const setupMongoose = () => {
-    mongoose.connect(process.env.MONGO_URL || "mongodb+srv://vk281977:jPDElHlz4qUIvE1d@cluster0.ywncs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect(process.env.MONGO_URL || "mongodb+srv://vk281977:jPDElHlz4qUIvE1d@cluster0.ywncs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000 // Timeout after 5 seconds
+    })
         .then(() => console.log('MongoDB connected'))
         .catch(err => console.error(err));
 }
