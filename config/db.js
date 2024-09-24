a697e1f72f440fba8e6e16cc2f25e3da23f1d30a
dexterflex <vk281977@gmail.com>
2024-09-24 13:06:26 +0530

    db bug remove

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const setupMongoose = () => {
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URL || "mongodb+srv://vk281977:jPDElHlz4qUIvE1d@cluster0.ywncs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         .then(() => console.log('MongoDB connected'))
         .catch(err => console.error(err));
 }
