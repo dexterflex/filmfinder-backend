7f415fde1f7e69c86a902595e4a9019ce96decc4
VIVEK KUMAR <vk281977@gmail.com>
2024-09-24 21:58:39 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,13 @@ export const signin = async (req, res, next) => {
     try {
         const { email, password } = req.body;
         const { user, token } = await UserRepository.login({ email, password });
-        res.cookie('token', token, { maxAge: 2 * 24 * 60 * 60 * 1000 });
+        // Set cookie
+        res.cookie('token', token, {
+            maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days
+            httpOnly: true, // Makes the cookie inaccessible to JavaScript
+            secure: true, // Set to true to send cookies over HTTPS only
+            sameSite: 'None' // Adjust based on your requirements (e.g., 'None' for cross-origin)
+        });
         res.status(200).send({ success: true, msg: 'User logged in successfully', user, token });
     } catch (error) {
         next(error);
