c778310ab80a236d805891fe194a149cbe089139
dexterflex <vk281977@gmail.com>
2024-09-24 16:14:38 +0530

    h

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import { ApplicationError } from '../middlewares/errorHandler.js';
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.
+            MONGODB_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed', error);
